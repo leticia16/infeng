e112f07eaf95c033a766589ab7f58e1b870b22a8
wesley <wesleydos504@gmail.com>
2021-01-05 10:49:33 -0300

    project structure

--- Evaluator.py
+++ Evaluator.py
@@ -1,6 +1,6 @@
-from infeng.scanner import tokens, Scanner
-from infeng.parser import Parser
-from .Value import Value
+from scanner import tokens, Scanner
+from parser import Parser
+from Value import Value
 
 def asking(value):
     i = input(f'What is the value of {value.name}? ')
--- parser/Parser.py
+++ parser/Parser.py
@@ -1,4 +1,4 @@
-from infeng.scanner import tokens
+from scanner import tokens
 
 class Parser(object):
     pritority = {
